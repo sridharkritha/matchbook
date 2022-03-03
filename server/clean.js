da213dda83981235d7da3a748e2c4c94be689a77
Sridhar Krishnan <sridhar.krishnan@realisticgames.co.uk>
2022-03-03 06:49:21 +0000

    update

--- server/clean.js
+++ server/clean.js
@@ -1,14 +1,14 @@
 (function () {
 	const fs = require('fs').promises; // promise version of require('fs');
 
-	async function cleanDirectory(directoryName) {
-		const result = await fs.readdir(directoryName).then((files) => Promise.all(files.map(file => fs.unlink(file))));
-		console.log(result);
-		// await Promise.all(files.map(file => fs.promises.unlink(path.join("./data-Report", file))));
+	async function cleanDirectory(directory) {
+		try {
+			await fs.readdir(directory).then((files) => Promise.all(files.map(file => fs.unlink(`${directory}/${file}`))));
+		} catch(err) {
+			console.log(err);
+		}
 	}
 
 	cleanDirectory("./data-Report");
-
-
 
 }()); // namespace
